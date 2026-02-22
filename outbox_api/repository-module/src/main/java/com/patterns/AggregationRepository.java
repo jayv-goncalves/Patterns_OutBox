977098ac2e87cd440f693bbed51931780dd0da60
Joao Vitor de Souza <joaovsgcontato@gmail.com>
2026-02-22 17:35:45 -0300

    refact: remove shared-modules

--- outbox_api/repository-module/src/main/java/com/patterns/AggregationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.patterns;
-
-import com.patterns.entity.UserAggregationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AggregationRepository extends JpaRepository<UserAggregationEntity, Long> {
-
-    UserAggregationEntity findById(long id);
-}
--- outbox_api/repository-module/src/main/java/com/patterns/GlobalAggregationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.patterns;
-
-import com.patterns.entity.GlobalAggregationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GlobalAggregationRepository extends JpaRepository<GlobalAggregationEntity, Long> {
-
-    GlobalAggregationEntity findById(long id);
-}
